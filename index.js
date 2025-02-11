a5ae19d5b6e608dde5e451c01dab42f268ef307b
Hilton-Borah <hiltonborah123@gmail.com>
2025-02-12 04:40:28 +0530

    bjb

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 // Socket.io for chat
 const io = new Server(server, {
   cors: {
-    origin: 'https://video-calling-hai.netlify.app/3000', // Allow React app to connect
+    origin: 'https://video-calling-hai.netlify.app/', // Allow React app to connect
     methods: ['GET', 'POST'],
   },
 });
